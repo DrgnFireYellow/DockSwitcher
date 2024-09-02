2d73727307c033a1d9a03ecba82d722399cc797a
DrgnFireYellow <DrgnFireYellow@users.noreply.github.com>
2024-09-02 16:41:53 -0400

    Add icon and fix menu

--- statusbar.py
+++ statusbar.py
@@ -1,11 +1,15 @@
 import rumps
 import main
+import copy
 
 class StatusBarApp(rumps.App):
-    def __init__(self, *args):
-        super().__init__(*args)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.add_profiles()
-        
+    
+    def create_load_lambda(self, item):
+        return lambda _ : main.load(item)
+    
     @rumps.clicked("Save Profile")
     def save(self, _):
         name = rumps.Window(title="What should this profile be called?", cancel=True).run().text
@@ -18,12 +22,12 @@ class StatusBarApp(rumps.App):
             del self.menu[item]
         for item in items:
             try:
-                self.menu.insert_before("Save Profile", rumps.MenuItem(f"Load '{item}'", lambda _: main.load(item)))
+                self.menu.insert_before("Save Profile", rumps.MenuItem(f"Load '{item}'", lambda _, item=item: main.load(item)))
             except KeyError:
-                self.menu.add(rumps.MenuItem(f"Load '{item}'", lambda _: main.load(item)))
+                self.menu.add(rumps.MenuItem(f"Load '{item}'", lambda _, item=item: main.load(item)))
         self.menu.insert_after(f"Load '{items[-1]}'", rumps.separator)
 
 
 if __name__ == "__main__":
-    app = StatusBarApp("DockSwitcher")
+    app = StatusBarApp("DockSwitcher", icon="logo.svg")
     app.run()
